266216b67dfe8a7b2e07e302d7d468a0096e7930
Morganero <morganero1@gmail.com>
2022-10-27 16:13:50 -0500

    got availableSessions and current sessions to show

--- src/components/views/ClientViews.js
+++ src/components/views/ClientViews.js
@@ -1,3 +1,40 @@
+import { Outlet, Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { ClientSessionEdit } from "../sessions/ClientSessionEdit"
+import { AvailableSessionsList } from "../sessions/AvailableSessions"
+
+
 export const ClientViews= () => {
-    <></>
+
+	const localMogoUser = localStorage.getItem("mogo_user")
+    const mogoUserObject = JSON.parse(localMogoUser)
+    const [client, setClient] = useState({})
+    useEffect (
+        ()=> {
+            fetch(`http://localhost:8088/users?id=${mogoUserObject.id}`)
+            .then(response => response.json())
+            .then((data) => {
+                const user=data[0]
+                setClient(user)
+            }) 
+        }, [mogoUserObject.id]
+    )    
+
+
+	return (
+	<Routes>
+		<Route path="/" element= {
+			<>
+			<h1>Mogo Gym</h1>
+			<div>Great Day, {`${client.fullName}`}! Always remember, if it doesn't challenge you, it doesn't change you. </div>
+			
+			<Outlet />
+			
+			</>
+		} />
+		<Route path="available" element={ <AvailableSessionsList/> } />
+		<Route path="sessions" element={ <ClientSessionEdit /> } />
+		
+	</Routes>
+	)
 }
--- src/components/views/TrainerViews.js
+++ src/components/views/TrainerViews.js
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react"
+import { Outlet, Route, Routes } from "react-router-dom"
+import { ClientList } from "../clients/ClientList"
+import { TrainerAvailabilityCreate } from "../sessions/TrainerAvailibiltyCreate"
+
 export const TrainerViews = () => {
-    <></>
+
+    const localMogoUser = localStorage.getItem("mogo_user")
+    const mogoUserObject = JSON.parse(localMogoUser)
+    const [client, setClient] = useState({})
+    useEffect (
+        ()=> {
+            fetch(`http://localhost:8088/users?id=${mogoUserObject.id}`)
+            .then(response => response.json())
+            .then((data) => {
+                const user=data[0]
+                setClient(user)
+            }) 
+        }, [mogoUserObject.id]
+    ) 
+    return (
+        <Routes>    
+            <Route path="/" element= {
+                <>
+                <h1>Mogo Gym</h1>
+                <div> Great day, {`${client.fullName}`}! Train them to train smart!</div>
+                
+                <Outlet />
+                
+                </>
+            } />
+            <Route path="clients" element={ <ClientList /> } />
+            <Route path="available" element={ <TrainerAvailabilityCreate /> } />
+            
+        </Routes>
+        )
+    
 }
